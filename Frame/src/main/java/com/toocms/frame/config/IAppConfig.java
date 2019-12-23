3d7388d473aba72f16a658b0df87921f61770544
98562@qq.com <bay011645>
2019-12-23 20:39:23 +0800

    弃用getProgectFolder回调函数，已自动处理存储文件夹

--- Frame/src/main/java/com/toocms/frame/config/IAppConfig.java
+++ Frame/src/main/java/com/toocms/frame/config/IAppConfig.java
@@ -11,38 +11,40 @@ public interface IAppConfig {
      *
      * @return
      */
-    public boolean isShowTitleCenter();
+    boolean isShowTitleCenter();
 
     /**
      * 是否使用SnackBar
      *
      * @return
      */
-    public boolean isUseSnackBar();
+    boolean isUseSnackBar();
 
     /**
      * 获取项目文件夹名字
      *
      * @return
+     * @deprecated 已被弃用，将在以后某个版本中移除
      */
-    public String getProgectFolder();
+    @Deprecated
+    String getProgectFolder();
 
     /**
      * 获取友盟的AppKey
      *
      * @return
      */
-    public String getUmengAppkey();
+    String getUmengAppkey();
 
     /**
      * 获取友盟推送服务的Secret
      *
      * @return
      */
-    public String getUmengPushSecret();
+    String getUmengPushSecret();
 
     /**
      * 在WeApplication中做第三方Jar包的初始化操作
      */
-    public void initJarForWeApplication(WeApplication application);
+    void initJarForWeApplication(WeApplication application);
 }
