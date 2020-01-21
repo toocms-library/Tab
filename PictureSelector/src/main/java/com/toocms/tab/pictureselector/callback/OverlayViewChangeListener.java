9510f4f84370e63b15ef24afb2a60c1d6ce4fc03
TooCMS <98562@qq.com>
2020-01-21 17:55:42 +0800

    remove PictureSelector Moudle

--- PictureSelector/src/main/java/com/toocms/tab/pictureselector/callback/OverlayViewChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.toocms.tab.pictureselector.callback;
-
-import android.graphics.RectF;
-
-public interface OverlayViewChangeListener {
-
-    void onCropRectUpdated(RectF cropRect);
-
-}
