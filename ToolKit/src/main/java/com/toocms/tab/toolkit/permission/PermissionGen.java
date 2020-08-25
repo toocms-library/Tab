c18142ee11a3d34ac14ee26d70cddf90af6cd215
toocms <98562@qq.com>
2020-08-25 17:58:48 +0800

    优化MIUI系统请求权限时会执行请求失败回调的问题

--- ToolKit/src/main/java/com/toocms/tab/toolkit/permission/PermissionGen.java
+++ ToolKit/src/main/java/com/toocms/tab/toolkit/permission/PermissionGen.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Binder;
 import android.os.Build;
-import android.os.Process;
 
 import androidx.fragment.app.Fragment;
 
